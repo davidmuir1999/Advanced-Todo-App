2bce5de55e939f64622a7d462fee6ec240ed2fc8
davidmuir1999 <david.bme.muir2017@gmail.com>
2024-05-28 09:11:03 +0100

    Repo Init

---
+++ js/app.js
@@ -0,0 +1,36 @@
+import { addTodo, toggleTodo, editTodoItem } from './utils.js';
+
+const addButton = document.getElementById('add-btn');
+const todoInput = document.getElementById('new-todo');
+const todoList = document.getElementById('todo-list');
+
+addButton.addEventListener('click', () => {
+    addTodoItem();
+});
+
+todoInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        addTodoItem();
+    }
+});
+
+todoList.addEventListener('change', (e) => {
+    if (e.target.type === 'checkbox') {
+        const todoItem = e.target.parentElement;
+        toggleTodo(todoItem);
+    }
+});
+
+todoList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+        editTodoItem(e.target);
+    }
+});
+
+function addTodoItem() {
+    const todoText = todoInput.value;
+    if (todoText.trim() !== '') {
+        addTodo(todoText);
+        todoInput.value = '';
+    }
+}
---
+++ js/utils.js
@@ -0,0 +1,56 @@
+export function addTodo(todoText) {
+    const todoList = document.getElementById('todo-list');
+    const todoItem = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-grip-lines';
+
+    const taskText = document.createElement('span');
+    taskText.textContent = todoText;
+
+    todoItem.appendChild(icon);
+    todoItem.appendChild(taskText);
+    todoItem.appendChild(checkbox);
+
+    todoList.appendChild(todoItem);
+}
+
+export function toggleTodo(todoItem) {
+    todoItem.classList.toggle('completed');
+
+}
+
+export function editTodoItem(taskText) {
+    const todoItem = taskText.parentElement;
+    if (todoItem.classList.contains('completed')){
+        return;
+    }
+    
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'edit-input';
+    input.value = taskText.textContent;
+
+    taskText.replaceWith(input);
+    input.focus();
+
+    function saveEdit() {
+        const newTaskText = document.createElement('span');
+        newTaskText.textContent = input.value;
+        input.replaceWith(newTaskText);
+
+        newTaskText.addEventListener('click', () => {
+            editTodoItem(newTaskText);
+        });
+    }
+
+    input.addEventListener('blur', saveEdit);
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            saveEdit();
+        }
+    });
+}
